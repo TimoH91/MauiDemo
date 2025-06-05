116be00ca83a12437396870fc7cb2a1fa1342795
TimoH91 <timothyharris04@gmail.com>
2025-06-05 20:04:27 +0100

    Flex Layout

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -12,8 +12,10 @@
             navPage.BarTextColor = Colors.Blue;
 
             var navPage2 = new NavigationPage(new StackLayoutPageDemo());
+            var navPage3 = new NavigationPage(new GridLayoutDemo());
 
-            MainPage = new NavigationPage(new GridLayoutDemo());
+            MainPage = new NavigationPage(new FlexLayoutDemo());
+            
         }
     }
 }
