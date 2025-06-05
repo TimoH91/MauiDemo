27f510986062081fc7de903ca3bc5e76487af814
TimoH91 <timothyharris04@gmail.com>
2025-06-05 13:39:47 +0100

    Add project files.

---
+++ MauiDemo/MinhaPagina.xaml.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+
+namespace MauiDemo;
+
+public partial class MinhaPagina : ContentPage
+{
+	public MinhaPagina()
+	{
+		InitializeComponent();
+	}
+
+	private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
+}
